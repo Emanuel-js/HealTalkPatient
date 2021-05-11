e6ed8b2b7eb9f1d988384f8c9ff5805fb40e21df
Emanuel-js <etioaman1960@gmail.com>
2021-05-11 18:24:05 +0300

    final project schema

---
+++ android/app/src/main/kotlin/com/example/healTalkpatient/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.healTalkpatient
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HealTalk());
+}
+
+class HealTalk extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("HealTalk"),
+        ),
+        body: Center(
+          child: Text("Let Start!!"),
+        ),
+      ),
+    );
+  }
+}
