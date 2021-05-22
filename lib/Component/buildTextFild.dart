33b4d82efa5a752d6ef5ffb7bb644899eeea0650
Emanuel-js <etioaman1960@gmail.com>
2021-05-22 20:03:44 +0300

    update login screen

---
+++ lib/Component/buildTextFild.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class BuildText extends StatelessWidget {
+  BuildText(
+      {this.isvisble,
+      this.iconPrifix,
+      this.iconSufix,
+      this.lebel,
+      this.controler,
+      this.onPress,
+      this.keybordType,
+      this.ispassword = true});
+
+  final isvisble;
+  final iconPrifix;
+  final iconSufix;
+  final controler;
+  final lebel;
+  final onPress;
+  final keybordType;
+  final ispassword;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: TextField(
+            controller: controler,
+            decoration: InputDecoration(
+              prefixIcon: iconPrifix,
+              suffixIcon: _textClear(),
+              labelText: lebel,
+              border: OutlineInputBorder(),
+            ),
+            keyboardType: keybordType,
+            textInputAction: TextInputAction.done,
+            obscureText: isvisble));
+  }
+
+  Widget _textClear() => ispassword
+      ? IconButton(
+          onPressed: () {
+            onPress();
+          },
+          icon: isvisble ? Icon(Icons.visibility_off) : Icon(Icons.visibility),
+        )
+      : controler.text.isEmpty
+          ? Container(
+              width: 0,
+            )
+          : IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () => controler.clear(),
+            );
+}
--- lib/Screen/loginScreen.dart
+++ lib/Screen/loginScreen.dart
@@ -1,37 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:healTalkpatient/Component/buildTextFild.dart';
 import "../index.dart";
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final emailControler = TextEditingController();
+  final passwordControler = TextEditingController();
+  bool isvisble = true;
+  bool isChecked = false;
+  @override
+  void initState() {
+    super.initState();
+    emailControler.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final colors = Appcolor();
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          children: [
-            //todo back arrow
+        child: Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                //todo back arrow
 
-            Container(
-              child: IconBtn(
-                color: colors.k_primerygreenColor,
-                icon: Icons.arrow_back,
-                onpress: () {
-                  Navigator.pop(context);
-                },
-              ),
-              padding: EdgeInsets.only(
-                right: MediaQuery.of(context).size.width - 50,
-              ),
+                Container(
+                  child: IconBtn(
+                    color: colors.k_primerygreenColor,
+                    icon: Icons.arrow_back,
+                    onpress: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  padding: EdgeInsets.only(
+                      right: MediaQuery.of(context).size.width - 50, top: 20),
+                ),
+                //todo logo
+                Image.asset(
+                  "asset/img/logo.png",
+                  width: MediaQuery.of(context).size.width,
+                ),
+                //todo email
+                Container(
+                  padding: EdgeInsets.all(30),
+                  child: BuildText(
+                    lebel: "Email",
+                    ispassword: false,
+                    controler: emailControler,
+                    iconPrifix: Icon(Icons.email),
+                    isvisble: false,
+                  ),
+                ),
+                //todo password
+                Container(
+                  padding: EdgeInsets.all(30),
+                  child: BuildText(
+                    lebel: "Password",
+                    ispassword: true,
+                    controler: passwordControler,
+                    isvisble: isvisble,
+                    onPress: () {
+                      setState(() {
+                        isvisble = !isvisble;
+                      });
+                    },
+                  ),
+                ),
+                // check box
+                CheckboxListTile(
+                  title: Text("Remember me"),
+                  value: isChecked,
+                  checkColor: colors.k_white,
+                  onChanged: (newValue) {
+                    setState(() {
+                      isChecked = newValue;
+                    });
+                  },
+                  controlAffinity: ListTileControlAffinity.leading,
+                ),
+                //todo button
+                SizedBox(
+                  height: 20,
+                ),
+                Button1(
+                  color: colors.k_primerygreenColor,
+                  text: "login",
+                  onpress: () => {
+                    print(
+                        "Email: ${emailControler.text} \n password: ${passwordControler.text}")
+                  },
+                )
+              ],
             ),
-            //todo logo
-            Image.asset(
-              "asset/img/logo.png",
-              width: MediaQuery.of(context).size.width,
-            ),
-            //todo email
-
-            //todo password
-          ],
+          ),
         ),
       ),
     );
